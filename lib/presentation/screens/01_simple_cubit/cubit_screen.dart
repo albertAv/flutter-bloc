5e34392d49658d72e653c1fce86e2335b043bff6
Alberto Avantes <alberto@got-itsolutions.com>
2024-10-17 13:10:53 -0600

    ejercicio cubit simple

--- lib/presentation/screens/01_simple_cubit/cubit_screen.dart
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -1,17 +1,34 @@
+import 'package:blocs_app/config/config.dart';
+import 'package:blocs_app/presentation/blocs/blocs.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitScreen extends StatelessWidget {
   const CubitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final usernameCubit = context.watch<UsernameCubit>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: BlocBuilder<UsernameCubit, String>(builder: (context, state) {
+          return Text(state);
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //acceder al cubit pero sin evaluacion de los cambios sobre este
+          // final UsernameCubit = context.read<UsernameCubit>();
+          context.read<UsernameCubit>().setUsername(RandomGenerator.getRandomName());
+          // usernameCubit.setUsername(
+          //   RandomGenerator.getRandomName(),
+          // );
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
